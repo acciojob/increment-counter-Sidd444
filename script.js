eba7e8f3ecfa78faf3b636d2695799eb02ef55dc
Sidd444 <123827703+Sidd444@users.noreply.github.com>
2023-11-22 22:42:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+function increaseCount(){
+var currEle=document.getElementById("counter");
+var currNo=Number.parseInt(currEle.innerText);
+alert(currNo++);
+currEle.innerText=currNo;
+}
